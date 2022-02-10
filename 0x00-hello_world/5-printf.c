8ce036170d8d1bb5f8c30bccd7473dd0d74b599c
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-10 18:24:10 -0500

    console message with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+	* main - print the message with printf
+	* Return: 0 if exited correctly, non-zero if catch
+	*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
