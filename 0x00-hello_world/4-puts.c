36d60c7cbf4e3708977cfbe8f539549a50ce1974
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-10 17:20:27 -0500

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+ int main() {
+     // here crete a variables
+     char message[] = "Programming is like building a multilingual puzzle\n";
+    
+    // here print the message
+     puts(message);
+
+     return 0;
+}
