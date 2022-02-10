14fbe9e729a1901fe379154c9044f6462056f026
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-10 18:08:58 -0500

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+	* main - print the message "Programming is like building a multilingual puzzle
+	* Return: 0 if exited correctly, non-zero if catch
+	*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
