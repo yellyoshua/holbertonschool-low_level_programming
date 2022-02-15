fe0d4fcfe509f7b24f1cfbaf2a8a98a49f37dd09
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-15 15:59:17 -0500

    fix 10 rows

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
