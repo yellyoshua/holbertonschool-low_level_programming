fb8d362e40812e0ca3fffdb1d1aec9e76216c08c
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-15 20:48:39 -0500

    fix print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,17 @@
 	*/
 int print_last_digit(int n)
 {
+	int result;
+
 	if (n < 0)
 	{
-		return (-n % 10);
-	}
+		result = -n % 10;
+		_putchar(result + '0');
 
-	return (n % 10);
+		return (result);
+	}
+	result = n % 10;
+	_putchar(result + '0');
+
+	return (result);
 }
