4e3744ccf73e99e63b87adadd65c09e1dfd843a9
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-15 22:21:24 -0500

    create func sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+	* add - func that return
+	*	a sum of two values
+	* @num1: number for sum
+	*	@num2: number for sum
+	* Return: Always 0 (Sucess)
+	*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
