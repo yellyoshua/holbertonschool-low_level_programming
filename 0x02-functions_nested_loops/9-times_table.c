b32562de73efeca115afa39089fa9a32aeca7231
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-15 21:56:50 -0500

    multiplication tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+	*	times_table - prints multiplication table
+	* Return: Always 0 (Success)
+	*/
+void times_table(void)
+{
+	int rows;
+	int cols;
+	int result;
+
+	for (rows = 0; rows < 10; rows++)
+	{
+		for (cols = 0; cols < 10; cols++)
+		{
+			result = cols * rows;
+
+			if (result > 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			}
+			if (!(cols + 1 == 10))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
