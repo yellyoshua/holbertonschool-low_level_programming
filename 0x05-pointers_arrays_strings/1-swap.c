835b1e56dd78259e46c27d848cd2aed9a5bbf442
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-21 17:01:51 -0500

    add func that swap values between arguments

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+	*	swap_int - swap values between arguments
+	*	@a: first argument number
+	*	@b: second argument number
+	*/
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
