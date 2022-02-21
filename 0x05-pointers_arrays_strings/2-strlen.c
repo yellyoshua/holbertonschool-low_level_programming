56110044bb81c91c84f589f98e2cc58e890d4287
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-21 17:35:05 -0500

    linter

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,12 @@
 	*/
 int _strlen(char *s)
 {
-  int i, c;
+	int i, c;
 
-  c = 0;
-  for (i = 0; s[i] != '\0'; i++)
-  {
-    c++;
-  }
-  return (c);
+	c = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c++;
+	}
+	return (c);
 }
