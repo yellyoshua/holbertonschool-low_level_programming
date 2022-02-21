6941f21d7905079d1adacb042045b3d5dbe6fa7f
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-21 17:23:14 -0500

    add func that return lenght of a text

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+	*	_strlen - return lenght of a text
+	*	@s: text
+	*	Return: Always 0 (Success)
+	*/
+int _strlen(char *s)
+{
+  int i, c;
+
+  c = 0;
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    c++;
+  }
+  return (c);
+}
