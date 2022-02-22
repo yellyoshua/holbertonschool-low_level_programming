7681643dbf1cf9144d1be6f482b640f1b6f3e3ff
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-21 22:29:05 -0500

    add func that prints n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array
+ * @a: arr of nums
+ * @n: # elements to print
+ */
+void print_array(int *a, int n)
+{
+  int i;
+
+  if (n <= 0)
+  {
+    printf("\n");
+  }
+  for (i = 0; i < n; i++)
+  {
+    if (i == n - 1)
+    {
+      printf("%d\n", a[i]);
+      break;
+    }
+    printf("%d, ", a[i]);
+  }
+}
