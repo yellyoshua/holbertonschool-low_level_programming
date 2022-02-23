7462111a790d1d916a659fa5d1bb52ac510bbe57
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-23 12:34:02 -0500

    add func that concat strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concat strings
+ * @dest: text
+ * @src: text
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	c = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		c++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
+
+	return (dest);
+}
