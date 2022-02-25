4079f2a81d9e2f75510039aeb3a7110daa78dd0b
yellyoshua <usuario1529yoshua@hotmail.com>
2022-02-25 17:57:02 -0500

    add func that convert lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * string_toupper - string to uppercase.
+ * @s: string
+ * Return: Always 0
+ */
+char *string_toupper(char *s)
+{
+  int i;
+  for (i = 0; s[i]; i++)
+  {
+    if (s[i] >= 'a' && s[i] <= 'z')
+      s[i] -= 32;
+  }
+  return (s);
+}
