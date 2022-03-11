40935d828f03a241ae131f9aa273ebc71b969626
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-11 11:01:14 -0500

    linter

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: type char str.
  * @s2: type char str
  * @n: size in bytes of s2
@@ -17,9 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			len1 = 0,
 			len2 = 0;
 
-	if (s1 == NULL)
-		return (NULL);
-	if (s2 == NULL)
+	if (!s1 || !s2)
 		return (NULL);
 
 	while (s1 && s1[len1])
