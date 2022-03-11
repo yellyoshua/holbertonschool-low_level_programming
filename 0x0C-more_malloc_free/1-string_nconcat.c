1bf5d21d1c1d96c049dda2b836b896b7d7530aef
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-11 10:58:15 -0500

    fix func that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -46,14 +46,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		for (j = 0; j < n; j++)
-		{
 			concat[i + j] = s2[j];
-			if ((j + 1) == n)
-			{
-				concat[i + j + 1] = '\n';
-			}
-		}
 	}
+
+	concat[i + j + 1] = '\0';
 
 	return (concat);
 }
