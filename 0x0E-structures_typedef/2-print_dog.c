ed37fc41fe09bffba36f81625ff0b662765339a3
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-15 23:13:42 -0500

    fix console result

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,5 @@ void print_dog(struct dog *d)
 	if (!d->owner)
 		d->owner = "(nil)";
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
