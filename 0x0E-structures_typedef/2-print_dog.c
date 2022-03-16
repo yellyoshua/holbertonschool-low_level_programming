23c7f11301a1a29ea428281753a66d0ec48357cf
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-15 23:09:54 -0500

    add func that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	if (!d->name)
+		d->name = "(nil)";
+	if (d->owner)
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
