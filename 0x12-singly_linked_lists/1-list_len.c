582377e14a8ab455bf8d1f8c8e378e30832df2a7
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-29 20:45:49 -0500

    add func that return the length of a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number
+ *  of elements in a linked list_t list.
+ * @h: linked list
+ * Return: Always 0 (Success)
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len_of_structure = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len_of_structure++;
+	}
+	return (len_of_structure);
+}
