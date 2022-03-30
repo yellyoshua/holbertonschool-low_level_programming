fa452bdfc4d8c40e9256fc237f63a66ed5591965
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-29 20:47:33 -0500

    linter

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,11 +2,10 @@
 
 /**
  * list_len - returns the number
- *  of elements in a linked list_t list.
+ * of elements in a linked list_t list.
  * @h: linked list
  * Return: Always 0 (Success)
  */
-
 size_t list_len(const list_t *h)
 {
 	size_t len_of_structure = 0;
@@ -16,5 +15,6 @@ size_t list_len(const list_t *h)
 		h = h->next;
 		len_of_structure++;
 	}
+
 	return (len_of_structure);
 }
