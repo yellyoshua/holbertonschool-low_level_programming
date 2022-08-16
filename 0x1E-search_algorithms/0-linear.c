ded4c6f7ff191b6a6325a7a7e5d962e813087af4
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-15 19:47:00 -0500

    add search algoriths project

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - Searches for a value in an array
+  *                 of integers using linear search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: Always 0
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,13 @@
+# 0x1E. C - Search Algorithms
+
+In this section, we will cover the following topics:
+- [Binary Search](#binary-search)
+- [Linear Search](#linear-search)
+- [Binary Search Tree](#binary-search-tree)
+- [Hash Table](#hash-table)
+- [Trie](#trie)
+- [Graph Search](#graph-search)
+- [A* Search](#a-star-search)
+- [Dijkstra's Algorithm](#dijkstras-algorithm)
+- [Bellman-Ford Algorithm](#bellman-ford-algorithm)
+- [Floyd-Warshall Algorithm](#floyd-warshall-algorithm)
